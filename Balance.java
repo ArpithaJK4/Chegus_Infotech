2deadc6d18dd3c7c5132b6817b8355d632560719
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2025-03-27 22:23:51 +0530

    Update Balance.java

--- Balance.java
+++ Balance.java
@@ -6,7 +6,7 @@ public class Balancedd {
         for (char currentChar : inputStr.toCharArray()) {
             if (currentChar == brackets[0] || currentChar == brackets[2] || currentChar == brackets[4]) {
                 balanceCounter++;
-            } else if (currentChar == brackets[1] || currentChar == brackets[3] || currentChar == brackets[5]) {
+            } else if (currentChar == brackets[1] || currentChar == brackets[3] || currentChar == brackets[5])   {
                 balanceCounter--;
                 if (balanceCounter < 0) {
                     return "Unbalanced";
